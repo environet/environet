f2bc988962a287b140e46a5038291e563a70a3b8
adam.balint <adam.balint@srg.hu>
2020-03-30 16:50:36 +0200

    Do not use realpath for configuration files which didn't created yet

--- sys/Commands/DistNode/Install.php
+++ sys/Commands/DistNode/Install.php
@@ -27,7 +27,7 @@ class Install extends BaseCommand {
 	 * @return int
 	 */
 	public function run($arguments): int {
-		$configFilePath = realpath(SRC_PATH.'/conf/conf.local.ini');
+		$configFilePath = SRC_PATH.'/conf/conf.local.ini';
 
 		if (file_exists($configFilePath)) {
 			$continue = $this->console->askYesNo("Local configuration file already exists: $configFilePath. Do you want to overwrite it?", false);
@@ -92,7 +92,7 @@ class Install extends BaseCommand {
 		file_put_contents($configFilePath, $iniContent);
 
 		$this->console->writeLineBreak();
-		$this->console->writeLine("The configuration has been saved to " . $configFilePath, Console::COLOR_GREEN);
+		$this->console->writeLine("The configuration has been saved to " . realpath($configFilePath), Console::COLOR_GREEN);
 
 		return 0;
 	}
