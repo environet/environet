5899bc7f0fe0330f803ed585c0726c24e2cba060
Attila Polákovics <attila.polakovi@gmail.com>
2020-04-17 11:36:40 +0200

    acl sql syntax fix

--- database/data_acl.sql
+++ database/data_acl.sql
@@ -1,20 +1,18 @@
 create table if not exists measurement_access_rules
 (
-    id serial not null
-        constraint measurement_access_rules_pk
-			primary key
-	operator_id integer,
-	monitoringpoint_selector text,
-	observed_property_selector text,
+	id                         serial not null
+		constraint measurement_access_rules_pk primary key,
+	operator_id                integer,
+	monitoringpoint_selector   text,
+	observed_property_selector text
 
 );
 
 create table group_measurement_access_rules
 (
-    id serial not null
-		constraint group_measurement_access_rules_pk
-			primary key
+	id                         serial not null
+		constraint group_measurement_access_rules_pk primary key,
 	measurement_access_rule_id integer,
-	group_id integer,
-	interval date,
+	group_id                   integer,
+	interval                   date
 );
