d32e5b33362d8fe84b163bae69e7a3179d5211c9
adam.balint <adam.balint@srg.hu>
2020-04-29 11:47:42 +0200

    Handle already migrated migrations

--- sys/Commands/DistNode/MigrateDb.php
+++ sys/Commands/DistNode/MigrateDb.php
@@ -4,6 +4,10 @@
 namespace Environet\Sys\Commands\DistNode;
 
 use Environet\Sys\Commands\Console;
+use Environet\Sys\General\Db\Connection;
+use Environet\Sys\General\Exceptions\InvalidConfigurationException;
+use Environet\Sys\General\Exceptions\QueryException;
+use PDO;
 
 /**
  * Class MigrateDb
@@ -14,6 +18,24 @@ use Environet\Sys\Commands\Console;
  * @author  SRG Group <dev@srg.hu>
  */
 class MigrateDb extends DbCommand {
+
+	/**
+	 * @var Connection|string|null
+	 */
+	protected $connection;
+
+
+	/**
+	 * MigrateDb constructor.
+	 *
+	 * @param Console $console
+	 *
+	 * @throws InvalidConfigurationException
+	 */
+	public function __construct(Console $console) {
+		parent::__construct($console);
+		$this->connection = Connection::getInstance();
+	}
 
 
 	/**
@@ -26,34 +48,75 @@ class MigrateDb extends DbCommand {
 	 * @return int
 	 */
 	public function run($arguments): int {
-		$output = [];
-		$exitCode = $this->createCrudPermissions($output);
-		if ($exitCode > 0) {
-			echo implode("\n", $output);
-			return $exitCode;
-		}
+		$migrations = [
+			'createCrudPermissions',
+			'createDataAclTables',
+			'createUploadPermissions'
+		];
 
-		echo implode("\n", $output);
+		//Run each migrations, and log results
+		foreach ($migrations as $migration) {
+			$output = [];
+			$exitCode = $this->{$migration}($output);
 
-		$this->console->writeLine('Crud permissions created successfully', Console::COLOR_GREEN);
-
-		$exitCode = $this->createDataAclTables($output);
-		if ($exitCode > 0) {
-			echo implode("\n", $output);
-			return $exitCode;
+			if ($exitCode === -1) {
+				//Already migrated
+				$this->console->writeLine("$migration: Already migrated", Console::COLOR_YELLOW);
+			} elseif ($exitCode > 0) {
+				//Error during migration
+				$this->console->writeLine("$migration: Errors:", Console::COLOR_RED);
+				foreach ($output as $item) {
+					$this->console->writeLine("$item");
+				}
+			} else {
+				//Success
+				$this->console->writeLine("$migration: Done", Console::COLOR_GREEN);
+			}
+			$this->console->writeLineBreak();
 		}
 
 		return $exitCode;
 	}
 
 
+	/**
+	 * Create CRUD permissions
+	 *
+	 * @param array $output
+	 *
+	 * @return int
+	 * @throws QueryException
+	 */
 	private function createCrudPermissions(array &$output): int {
+		//Check if a permission is already added
+		$count = $this->connection->runQuery(
+			'SELECT COUNT(*) FROM public.permissions WHERE name = :loginPermission',
+			['loginPermission' => 'admin.login']
+		)->fetch(PDO::FETCH_COLUMN);
+		if ($count) {
+			return -1;
+		}
 		$schemaPath = SRC_PATH . '/database/create_crud_permissions.sql';
 		return $this->runSqlFile($schemaPath, $output);
 	}
 
 
+	/**
+	 * Create acl tables
+	 * @param array $output
+	 *
+	 * @return int
+	 * @throws QueryException
+	 */
 	private function createDataAclTables(array &$output): int {
+		//Check if a table already exists
+		$count = $this->connection->runQuery(
+			'SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = :tableName;',
+			['tableName' => 'measurement_access_rules']
+		)->fetch(PDO::FETCH_COLUMN);
+		if ($count) {
+			return -1;
+		}
 		$schemaPath = SRC_PATH . '/database/data_acl.sql';
 		return $this->runSqlFile($schemaPath, $output);
 	}
