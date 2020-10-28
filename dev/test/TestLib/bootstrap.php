81ff5231df1a22bc6060fc617a0419e4be5d34e9
adam.balint <adam.balint@srg.hu>
2020-10-28 13:51:02 +0100

    --enh XmlParser refactoring

--- dev/test/TestLib/bootstrap.php
+++ dev/test/TestLib/bootstrap.php
@@ -4,5 +4,8 @@
 /** @var Loader $autoloader */
 define('SRC_PATH', realpath(__DIR__.'/../../..'));
 
+define('TEST_RESOURCES_PATH', realpath(__DIR__.'/../resources'));
+define('CONFIGURATION_PATH', realpath(TEST_RESOURCES_PATH.'/configurations'));
+
 $autoloader = include __DIR__ . '/../../../sys/init_autoloader.inc.php';
 $autoloader = include __DIR__ . '/../../../sys/constants.inc.php';
