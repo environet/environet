e267a6ba7d8dbf5e65c55a3e0f2f691ef0ba2dd3
adam.balint <adam.balint@srg.hu>
2021-05-14 11:06:39 +0200

    --fix Drop database before database init command

--- database/schema.sql
+++ database/schema.sql
@@ -16,24 +16,13 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-\connect environet
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 
 
 --
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
 --
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
 
 COMMENT ON SCHEMA public IS 'standard public schema';
 
