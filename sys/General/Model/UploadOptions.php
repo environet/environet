e41bfdf8c72748e6969ad5fbb7939efd3941e16a
adam.balint <adam.balint@srg.hu>
2025-02-17 09:46:51 +0100

    fix: Check if upload-options is set
    Refs: DAREFFORT-319

--- sys/General/Model/UploadOptions.php
+++ sys/General/Model/UploadOptions.php
@@ -29,23 +29,25 @@ class UploadOptions {
 		if ($parsedXml !== null) {
 			//Set values from XML. If not set, default values will be used
 			$uploadOptions = $parsedXml->xpath('/environet:UploadData/environet:UploadOptions')[0] ?? null;
-			foreach ($uploadOptions->xpath('*') as $option) {
-				$optionName = lcfirst($option->getName());
-				if (!property_exists($this, $optionName)) {
-					throw new UploadException(408);
-				}
-				$defaultValue = $this->{$optionName};
-				$optionValue = (string) $option;
-				//Cast to the type of the default value
-				switch (gettype($defaultValue)) {
-					case 'boolean':
-						$optionValue = filter_var($optionValue, FILTER_VALIDATE_BOOLEAN);
-						break;
-					default:
-						break;
-				}
+			if ($uploadOptions) {
+				foreach ($uploadOptions->xpath('*') as $option) {
+					$optionName = lcfirst($option->getName());
+					if (!property_exists($this, $optionName)) {
+						throw new UploadException(408);
+					}
+					$defaultValue = $this->{$optionName};
+					$optionValue = (string) $option;
+					//Cast to the type of the default value
+					switch (gettype($defaultValue)) {
+						case 'boolean':
+							$optionValue = filter_var($optionValue, FILTER_VALIDATE_BOOLEAN);
+							break;
+						default:
+							break;
+					}
 
-				$this->{$optionName} = $optionValue;
+					$this->{$optionName} = $optionValue;
+				}
 			}
 		}
 
